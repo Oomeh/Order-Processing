8a6de6a1d90b26e23acbbe8533410f86f8827c1b
Blake Vance <nsdnmplz@gmail.com>
2017-11-27 14:22:57 -0500

    Merge origin/master
    Conflicts:
    	README.md

---
+++ src/orderprocessor/OrderProcessor.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orderprocessor;
+
+import java.io.File;
+
+/**
+ *
+ * @author bvance
+ */
+public class OrderProcessor {
+    /**
+     * 
+     * @param input
+     * @param output 
+     */
+    public OrderProcessor(File input, File output) {
+        
+    }
+    /**
+     * 
+     */
+    public OrderProcessor() {
+        File inputFile = new File("/Volumes/Shared Files/Orders.txt");
+        File outputFile = new File("OrdersProcessed.txt");
+        
+    }
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    /**
+     * 
+     * 
+     */
+    public void openFiles(){
+        
+    }
+    /**
+     * 
+     */
+    public void closeFiles(){
+        
+    }
+    /**
+     * 
+     */
+    public void readOrders(){
+        
+    }
+    /**
+     * 
+     */
+    public void write(){
+        
+    }
+}
